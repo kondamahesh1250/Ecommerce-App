8535a046c203ca4f19180635d4d85a8fa37ccb0e
k_mahesh <konda.mahesh1250@gmail.com>
2026-03-24 23:57:04 +0530

    first commit

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -26,7 +26,7 @@ function Login() {
   };
 
   const googleLogin = () => {
-    window.location.href = "https://ecommerce-app-h6fe.onrender.com/oauth2/authorization/google";
+    window.location.href = "https://ecommerce-app-66e7.onrender.com/oauth2/authorization/google";
   };
 
   return (
--- src/pages/OAuthSuccess.jsx
+++ src/pages/OAuthSuccess.jsx
@@ -2,35 +2,31 @@ import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 function OAuthSuccess() {
-
   const navigate = useNavigate();
 
- useEffect(() => {
-  console.log("OAuthSuccess loaded");
+  useEffect(() => {
+    console.log("OAuthSuccess loaded");
 
-  const params = new URLSearchParams(window.location.search);
+    const params = new URLSearchParams(window.location.search);
 
-  const token = params.get("token");
-  const role = params.get("role");
+    const token = params.get("token");
+    const role = params.get("role");
 
-  console.log("Token:", token);
-  console.log("Role:", role);
+    console.log("Token:", token);
+    console.log("Role:", role);
 
-  if (token) {
-    localStorage.setItem("token", token);
-    localStorage.setItem("role", role);
-    if (role === "ROLE_ADMIN") {
-        window.location.href="/admin/dashboard";
-        // navigate("/admin/dashboard");
+    if (token) {
+      localStorage.setItem("token", token);
+      localStorage.setItem("role", role);
+      if (role === "ROLE_ADMIN") {
+        window.location.href = "/admin/dashboard";
+      } else {
+        window.location.href = "/user/dashboard";
+      }
+    } else {
+      navigate("/");
     }
-    else {
-        // navigate("/user/dashboard");
-        window.location.href="/user/dashboard";
-    }
-  } else {
-    navigate("/");
-  }
-}, []);
+  }, []);
 
   return <h2>Logging in...</h2>;
 }
